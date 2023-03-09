8524e45f8b1da1083c58f44d1822bcc9e21f8d2f
happy_codder_is_back <fuckThisConfigStuff&fuckYou.com>
2023-03-09 18:56:13 +0200

    added some more counties

--- links.js
+++ links.js
@@ -7,5 +7,13 @@ const links = [
     judet: "Ilfov",
     pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-ILFOV-31-ianuarie-BAC2023-subiect-var-PDF.pdf",
     todo: true
-  }
+  },
+  { judet: "Botosani", pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-BOTOSANI-27ianuarie-BAC2023-subiect-var-PDF.pdf", }
+  , {
+    judet: "Maramures", pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2020/04/E-c-Simulare-MARAMURES-BAC2023-matematica-M-mate-info-subiect.pdf"
+  },
+  {
+    judet: "Vrancea", pdflink: "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_C_MA2.pdf"
+  },
+  { judet: "Timis", pdflink: "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_c_matematica_M_mate-info_simulare-var1.pdf" }
 ]
