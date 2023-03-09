aeb29804aa581181e2a28f5060b20d2a3336bbe9
happy_codder_is_back <fuckThisConfigStuff&fuckYou.com>
2023-03-09 16:33:13 +0200

    now has view in site and external config file

---
+++ links.js
@@ -0,0 +1,11 @@
+const links = [
+  {
+    judet: "Dolj",
+    pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2022/02/E-c-matematica-M-MATE-INFO-simulare-BAC-2023-DOLJ-februarie2023-subiect-var-PDF.pdf",
+    done: true
+  }, {
+    judet: "Ilfov",
+    pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-ILFOV-31-ianuarie-BAC2023-subiect-var-PDF.pdf",
+    todo: true
+  }
+]
