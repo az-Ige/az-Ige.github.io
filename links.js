6d21fa04f0ee87e7d4c2ff716f836689c7041dad
happy_codder_is_back <fuckThisConfigStuff&fuckYou.com>
2023-03-10 13:51:46 +0200

    Updated Vrancea

--- links.js
+++ links.js
@@ -1,19 +1,34 @@
 const links = [
   {
     judet: "Dolj",
-    pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2022/02/E-c-matematica-M-MATE-INFO-simulare-BAC-2023-DOLJ-februarie2023-subiect-var-PDF.pdf",
-    done: true
-  }, {
-    judet: "Ilfov",
-    pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-ILFOV-31-ianuarie-BAC2023-subiect-var-PDF.pdf",
-    todo: true
-  },
-  { judet: "Botosani", pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-BOTOSANI-27ianuarie-BAC2023-subiect-var-PDF.pdf", }
-  , {
-    judet: "Maramures", pdflink: "https://profesorjitaruionel.com/wp-content/uploads/2020/04/E-c-Simulare-MARAMURES-BAC2023-matematica-M-mate-info-subiect.pdf"
+    pdflink:
+      "https://profesorjitaruionel.com/wp-content/uploads/2022/02/E-c-matematica-M-MATE-INFO-simulare-BAC-2023-DOLJ-februarie2023-subiect-var-PDF.pdf",
+    done: true,
   },
   {
-    judet: "Vrancea", pdflink: "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_C_MA2.pdf"
+    judet: "Ilfov",
+    pdflink:
+      "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-ILFOV-31-ianuarie-BAC2023-subiect-var-PDF.pdf",
+    done: true,
   },
-  { judet: "Timis", pdflink: "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_c_matematica_M_mate-info_simulare-var1.pdf" }
-]
+  {
+    judet: "Botosani",
+    pdflink:
+      "https://profesorjitaruionel.com/wp-content/uploads/2023/01/E-c-matematica-M-MATE-INFO-simulare-BOTOSANI-27ianuarie-BAC2023-subiect-var-PDF.pdf",
+  },
+  {
+    judet: "Maramures",
+    pdflink:
+      "https://profesorjitaruionel.com/wp-content/uploads/2020/04/E-c-Simulare-MARAMURES-BAC2023-matematica-M-mate-info-subiect.pdf",
+  },
+  {
+    judet: "Vrancea",
+    pdflink: "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_C_MA2.pdf",
+    todo: true,
+  },
+  {
+    judet: "Timis",
+    pdflink:
+      "https://cdn.edupedu.ro/wp-content/uploads/2023/02/E_c_matematica_M_mate-info_simulare-var1.pdf",
+  },
+];
